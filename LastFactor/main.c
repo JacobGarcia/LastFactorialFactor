d8f37556144d65d2671b7226943cc9d2ec4f616f
Jacob Garcia <fatalraincloud@gmail.com>
2015-01-23 10:06:17 -0600

    Initial Commit

---
+++ LastFactor/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  LastFactor
+//
+//  Created by Mario Garcia on 1/23/15.
+//  Copyright (c) 2015 Jacob García. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
